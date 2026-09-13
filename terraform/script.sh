f3218a4238f58bcc1dd5f5ed31bdfd09be2aaddb
ericmolinam <srmolina5100@gmail.com>
2026-09-13 14:24:06 +0200

    feat: add health check response to index.html in user_data script

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo yum update -y && sudo yum install -y httpd
+echo "Healthy response from $(hostname)" | sudo tee /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
