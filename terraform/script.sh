59b7dda03f56383c0a0a18670c2b4d38942fbea8
ericmolinam <srmolina5100@gmail.com>
2026-09-14 18:54:11 +0200

    fix: add timestamp to script execution log for better tracking

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum update -y && sudo yum install -y httpd
 echo "Healthy response from $(hostname)" | sudo tee /var/www/html/index.html
+echo "Script executed on $(date)" | sudo tee -a /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
