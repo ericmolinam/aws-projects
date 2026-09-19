4147326b80bd859c9020cdcc3502eb83a4137242
ericmolinam <srmolina5100@gmail.com>
2026-09-19 20:54:47 +0200

    feat: create route table per subnet

--- lab-aws-eks/network-public.tf
+++ lab-aws-eks/network-public.tf
@@ -22,6 +22,8 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table" "public" {
+  for_each = local.public_subnets
+
   vpc_id = aws_vpc.this.id
 
   route {
@@ -30,7 +32,7 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "${local.env}-public-eks"
+    Name = "${local.env}-public-eks-${each.value.availability_zone}"
   }
 }
 
@@ -38,47 +40,47 @@ resource "aws_route_table_association" "public" {
   for_each = local.public_subnets
 
   subnet_id      = aws_subnet.public[each.key].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public[each.key].id
 }
 
-resource "aws_security_group" "public" {
-  name   = "${local.env}-public-eks"
-  vpc_id = aws_vpc.this.id
+# resource "aws_security_group" "public" {
+#   name   = "${local.env}-public-eks"
+#   vpc_id = aws_vpc.this.id
 
-  tags = {
-    Name = "${local.env}-public-eks"
-  }
-}
+#   tags = {
+#     Name = "${local.env}-public-eks"
+#   }
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  description       = "Allow SSH"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "ssh" {
+#   security_group_id = aws_security_group.public.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   description       = "Allow SSH"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  description       = "Allow HTTP"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+# resource "aws_vpc_security_group_ingress_rule" "http" {
+#   security_group_id = aws_security_group.public.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   description       = "Allow HTTP"
+#   from_port         = 80
+#   ip_protocol       = "tcp"
+#   to_port           = 80
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "https" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  description       = "Allow HTTPS"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+# resource "aws_vpc_security_group_ingress_rule" "https" {
+#   security_group_id = aws_security_group.public.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   description       = "Allow HTTPS"
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
 
-resource "aws_vpc_security_group_egress_rule" "public" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "public" {
+#   security_group_id = aws_security_group.public.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
