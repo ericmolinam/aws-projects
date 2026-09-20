c3fa630d36f1d70a03f02a00152c73a93577f649
ericmolinam <srmolina5100@gmail.com>
2026-09-20 15:57:26 +0200

    feat: update iam role names and add policy attachments for eks node groups

--- lab-aws-eks/iam.tf
+++ lab-aws-eks/iam.tf
@@ -1,5 +1,6 @@
+# IAM role and policy attachment for EKS cluster
 resource "aws_iam_role" "eks" {
-  name = "${local.env}-${local.eks_name}-eks-role"
+  name = "${local.env}-${local.eks_name}-eks-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -17,8 +18,43 @@ resource "aws_iam_role" "eks" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "eks" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks.name
 }
 
+# IAM role and policy attachment for EKS node groups
+resource "aws_iam_role" "nodes" {
+  name = "${local.env}-${local.eks_name}-eks-nodes"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+# This policy now includes AssumeRoleForPodIdentity for the Pod Identity Agent
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
