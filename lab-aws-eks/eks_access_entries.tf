33809cffe07034239bf3c4c323d4ddc56e64a0c9
ericmolinam <srmolina5100@gmail.com>
2026-09-26 15:43:39 +0200

    refactor: reorganize SSO role data blocks and update resource names for clarity

--- lab-aws-eks/eks_access_entries.tf
+++ lab-aws-eks/eks_access_entries.tf
@@ -1,25 +1,20 @@
-# locate SSO Roles dynamically
-data "aws_iam_roles" "sso_admin" {
+# AdministratorAccess
+
+data "aws_iam_roles" "sso_admin" { # locate SSO Roles dynamically
   name_regex  = "^AWSReservedSSO_AdministratorAccess_.*"
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
 
-data "aws_iam_roles" "sso_poweruser" {
-  name_regex  = "^AWSReservedSSO_PowerUserAccess_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
-
-# AdministratorAccess
-resource "aws_eks_access_entry" "admin_sso" {
+resource "aws_eks_access_entry" "sso_admin" {
   cluster_name  = aws_eks_cluster.eks.name
   principal_arn = tolist(data.aws_iam_roles.sso_admin.arns)[0]
   type          = "STANDARD"
 }
 
-resource "aws_eks_access_policy_association" "admin_sso" {
+resource "aws_eks_access_policy_association" "sso_admin" {
   cluster_name  = aws_eks_cluster.eks.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = aws_eks_access_entry.admin_sso.principal_arn
+  principal_arn = aws_eks_access_entry.sso_admin.principal_arn
 
   access_scope {
     type = "cluster"
@@ -27,17 +22,23 @@ resource "aws_eks_access_policy_association" "admin_sso" {
 }
 
 # PowerUserAccess
-resource "aws_eks_access_entry" "dev_sso" {
+
+data "aws_iam_roles" "sso_poweruser" { # locate SSO Roles dynamically
+  name_regex  = "^AWSReservedSSO_PowerUserAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
+resource "aws_eks_access_entry" "sso_poweruser" {
   cluster_name  = aws_eks_cluster.eks.name
   principal_arn = tolist(data.aws_iam_roles.sso_poweruser.arns)[0]
   type          = "STANDARD"
 }
 
-resource "aws_eks_access_policy_association" "dev_sso" {
+resource "aws_eks_access_policy_association" "sso_poweruser" {
   cluster_name = aws_eks_cluster.eks.name
   # AmazonEKSEditPolicy (read/write workloads) or AmazonEKSViewPolicy (read-only)
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
-  principal_arn = aws_eks_access_entry.dev_sso.principal_arn
+  principal_arn = aws_eks_access_entry.sso_poweruser.principal_arn
 
   access_scope {
     type = "cluster" # or "namespace", e.g. namespaces = ["default"]
